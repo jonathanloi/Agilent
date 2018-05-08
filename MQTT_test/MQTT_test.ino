801228f6298fda76af118c7e96b34c6827214574
jonathanloi <jonathanloi@users.noreply.github.com>
2018-05-08 17:12:06 +0800

    nope

--- MQTT_test/MQTT_test.ino
+++ MQTT_test/MQTT_test.ino
@@ -4,15 +4,10 @@
 
 #include <Thread.h>             // https://github.com/ivanseidel/ArduinoThread
 #include <ThreadController.h>
-extern "C" {
-  #include "user_interface.h"
-  #include "wpa2_enterprise.h"
-}
-#define MQTT_SERVER "127.0.0.1"
-//defines the WPA2-Enterprise details
-static const char* ssid = "spark";
-static const char* username = "jonatloi";
-static const char* password = "Jljon1999!";
+
+#define WIFI_SSID "JON"
+#define WIFI_PASS "jljon1999"
+#define MQTT_SERVER "broker.mqtt-dashboard.com"
 
 WiFiClient espClient;
 PubSubClient client(MQTT_SERVER, 1883, espClient);
@@ -28,31 +23,13 @@ void setup() {
   Serial.begin(115200);
 
   // Connect to WiFi
-  wifi_set_opmode(STATION_MODE);
-
-  struct station_config wifi_config;
-
-  memset(&wifi_config, 0, sizeof(wifi_config));
-  strcpy((char*)wifi_config.ssid, ssid);
-
-  wifi_station_set_config(&wifi_config);
-
-  wifi_station_clear_cert_key();
-  wifi_station_clear_enterprise_ca_cert();
-
-  wifi_station_set_wpa2_enterprise_auth(1);
-  wifi_station_set_enterprise_username((uint8*)username, strlen(username));
-  wifi_station_set_enterprise_password((uint8*)password, strlen(password));
-
-  wifi_station_connect();
+  Serial.print(s+"Connecting to WiFi: "+WIFI_SSID+" ");
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(s+" connected with IP: "+WiFi.localIP());
 
   // Connect to MQTT
   Serial.print(s+"Connecting to MQTT: "+MQTT_SERVER+" ... ");
