328863cae81f6164945c6c3323ce167d569af45c
jonathanloi <jonathanloi@users.noreply.github.com>
2018-05-09 10:42:28 +0800

    doesnt coonet to wifi?
    https://www.baldengineer.com/mqtt-tutorial.html

--- MQTT_test/MQTT_test.ino
+++ MQTT_test/MQTT_test.ino
@@ -1,102 +1,69 @@
-// This example uses an Adafruit Huzzah ESP8266
-// to connect to shiftr.io.
-//
-// You can check on your device after a successful
-// connection here: https://shiftr.io/try.
-//
-// by Joël Gähwiler
-// https://github.com/256dpi/arduino-mqtt
-
+#include <EEPROM.h>
 #include <ESP8266WiFi.h>
-#include <MQTT.h>
-extern "C" {
-  #include "user_interface.h"
-  #include "wpa2_enterprise.h"
+#include <PubSubClient.h>
+#include <Wire.h>
+//#include <Adafruit_INA219.h>
+ 
+// Connect to the WiFi
+const char* ssid = "JON";
+const char* password = "jljon1999";
+const char* mqtt_server = "127.0.0.1";
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
+ 
+const byte ledPin = 0; // Pin with LED on Adafruit Huzzah
+ 
+void callback(char* topic, byte* payload, unsigned int length) {
+ Serial.print("Message arrived [");
+ Serial.print(topic);
+ Serial.print("] ");
+ for (int i=0;i<length;i++) {
+  char receivedChar = (char)payload[i];
+  Serial.print(receivedChar);
+  if (receivedChar == '0')
+  // ESP8266 Huzzah outputs are "reversed"
+  digitalWrite(ledPin, HIGH);
+  if (receivedChar == '1')
+   digitalWrite(ledPin, LOW);
+  }
+  Serial.println();
 }
-
-static const char* ssid = "spark";
-static const char* username = "jonatloi";
-static const char* password = "Jljon1999!";
-
-
-WiFiClient net;
-MQTTClient client;
-
-unsigned long lastMillis = 0;
-
-void wificonnect(){
-   wifi_set_opmode(STATION_MODE);
-
-  struct station_config wifi_config;
-
-  memset(&wifi_config, 0, sizeof(wifi_config));
-  strcpy((char*)wifi_config.ssid, ssid);
-
-  wifi_station_set_config(&wifi_config);
-
-  wifi_station_clear_cert_key();
-  wifi_station_clear_enterprise_ca_cert();
-
-  wifi_station_set_wpa2_enterprise_auth(1);
-  wifi_station_set_enterprise_username((uint8*)username, strlen(username));
-  wifi_station_set_enterprise_password((uint8*)password, strlen(password));
-
-  wifi_station_connect();
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+ 
+ 
+void reconnect() {
+ // Loop until we're reconnected
+ while (!client.connected()) {
+ Serial.print("Attempting MQTT connection...");
+ // Attempt to connect
+ if (client.connect("ESP8266 Client")) {
+  Serial.println("connected");
+  // ... and subscribe to topic
+  client.subscribe("ledStatus");
+ } else {
+  Serial.print("failed, rc=");
+  Serial.print(client.state());
+  Serial.println(" try again in 5 seconds");
+  // Wait 5 seconds before retrying
+  delay(5000);
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  
-  }
-void connect() {
-  Serial.print("checking wifi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(1000);
-  }
-
-  Serial.print("\nconnecting...");
-  while (!client.connect("esp")) {
-    Serial.print(".");
-    delay(1000);
-  }
-
-  Serial.println("\nconnected!");
-  client.subscribe("/hello");
-  // client.unsubscribe("/hello");
+ }
 }
-
-void messageReceived(String &topic, String &payload) {
-  Serial.println("incoming: " + topic + " - " + payload);
+ 
+void setup()
+{
+ Serial.begin(9600);
+ 
+ client.setServer(mqtt_server, 1883);
+ client.setCallback(callback);
+ 
+ pinMode(ledPin, OUTPUT);
 }
-
-void setup() {
-  Serial.begin(115200);
-  wificonnect();
-
-  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
-  // You need to set the IP address directly.
-  client.begin("127.0.0.1", net);
-  client.onMessage(messageReceived);
-
-  connect();
-}
-
-void loop() {
-  client.loop();
-  delay(10);  // <- fixes some issues with WiFi stability
-
-  if (!client.connected()) {
-    connect();
-  }
-
-  // publish a message roughly every second.
-  if (millis() - lastMillis > 1000) {
-    lastMillis = millis();
-    client.publish("/hello", "world");
-  }
+ 
+void loop()
+{
+ if (!client.connected()) {
+  reconnect();
+ }
+ client.loop();
 }
