31302d728c0162e300faf05b90ab24238c890e15
jonathanloi <jonathanloi@users.noreply.github.com>
2018-05-31 10:52:12 +0800

    Testing out Chartjs

---
+++ WEBpage Test/js/main.js
@@ -0,0 +1,20 @@
+var ctx = document.getElementById('myChart').getContext("2d");
+let chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "My First dataset",
+            fill: false,
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [10, 20, 5, 2, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
