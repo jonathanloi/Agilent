a9546b8968304a349238ab637ac461b731fdbb7f
jonathanloi <jonathanloi@users.noreply.github.com>
2018-06-01 14:46:44 +0800

    Get works with hotspot

---
+++ GET_Test/GET_Test.ino
@@ -0,0 +1,42 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+ 
+void setup () {
+ 
+  Serial.begin(115200);
+  WiFi.begin("abcd", "6oM47!92");
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+ 
+    delay(1000);
+    Serial.println("Connecting..");
+ 
+  }
+  Serial.println("Connected to WiFi Network");
+ 
+}
+ 
+void loop() {
+ 
+  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+ 
+    HTTPClient http;  //Declare an object of class HTTPClient
+ 
+    http.begin("http://sgh721qbmq:1337/itrolley/35"); //Specify request destination
+ 
+    int httpCode = http.GET(); //Send the request
+ 
+    if (httpCode > 0) { //Check the returning code
+ 
+      String payload = http.getString();   //Get the request response payload
+      Serial.println(payload);             //Print the response payload
+ 
+    }else Serial.println("An error ocurred");
+ 
+    http.end();   //Close connection
+ 
+  }
+ 
+  delay(10000);    //Send a request every 10 seconds
+ 
+}
