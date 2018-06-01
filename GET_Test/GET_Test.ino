96f734d6640f558f9cc006e6e6ef72f519dd2fa2
jonathanloi <jonathanloi@users.noreply.github.com>
2018-06-01 15:35:20 +0800

    filtered the string payload down to only the values

--- GET_Test/GET_Test.ino
+++ GET_Test/GET_Test.ino
@@ -1,62 +1,47 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
 
-extern "C" {
-#include "user_interface.h"
-#include "wpa2_enterprise.h"
-}
-
-static const char* ssid = "spark";
-static const char* username = "jonatloi";
-static const char* password = "Jljon1999!";
  
 void setup () {
  
   Serial.begin(115200);
-  wifi_set_opmode(STATION_MODE);
-  struct station_config wifi_config;
-  memset(&wifi_config, 0, sizeof(wifi_config));
-  strcpy((char*)wifi_config.ssid, ssid);
-  wifi_station_set_config(&wifi_config);
-  wifi_station_clear_cert_key();
-  wifi_station_clear_enterprise_ca_cert();
-  wifi_station_set_wpa2_enterprise_auth(1);
-  //wifi_station_set_enterprise_identity((uint8*)username, strlen(username));
-  wifi_station_set_enterprise_username((uint8*)username, strlen(username));
-  wifi_station_set_enterprise_password((uint8*)password, strlen(password));
-  wifi_station_connect();
-  //WiFi.begin("abcd", "6oM47!92");
-  Serial.println("Connecting");
+  WiFi.begin("abcd", "6oM47!92");
+ 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(2000);
-    Serial.print(".");
+    delay(1000);
+    Serial.println("Connecting..");
  
   }
   Serial.println("Connected to WiFi Network");
  
 }
- 
 void loop() {
- 
-  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
- 
-    HTTPClient http;  //Declare an object of class HTTPClient
- 
-    http.begin("http://sgh721qbmq:1337/itrolley/35"); //Specify request destination
+
+  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status 
+   HTTPClient http;  //Declare an object of class HTTPClient
+
+    http.begin("http://sgh721qbmq:1337/itrolley/87"); //Specify request destination
  
     int httpCode = http.GET(); //Send the request
  
     if (httpCode > 0) { //Check the returning code
- 
-      String payload = http.getString();   //Get the request response payload
+     String payload = http.getString();   //Get the request response payload
       Serial.println(payload);             //Print the response payload
- 
+      StaticJsonBuffer<300> jBuffer;
+      JsonObject& jObject = jBuffer.parseObject(payload);
+      String nam = jObject["name"];
+      String tem = jObject["temperature"];
+      Serial.println(nam);
+      Serial.println(tem.toInt());
     }else Serial.println("An error ocurred");
  
-    http.end();   //Close connection
+   http.end();   //Close connection
  
   }
- 
+
   delay(10000);    //Send a request every 10 seconds
  
 }
+ 
+      
