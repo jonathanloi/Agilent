e1a66bf1f2c619a9b7befbb3054c881e22c9f6c2
jonathanloi <jonathanloi@users.noreply.github.com>
2018-06-01 14:53:55 +0800

    doesnt work with spark

--- GET_Test/GET_Test.ino
+++ GET_Test/GET_Test.ino
@@ -1,15 +1,35 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+
+extern "C" {
+#include "user_interface.h"
+#include "wpa2_enterprise.h"
+}
+
+static const char* ssid = "spark";
+static const char* username = "jonatloi";
+static const char* password = "Jljon1999!";
  
 void setup () {
  
   Serial.begin(115200);
-  WiFi.begin("abcd", "6oM47!92");
- 
+  wifi_set_opmode(STATION_MODE);
+  struct station_config wifi_config;
+  memset(&wifi_config, 0, sizeof(wifi_config));
+  strcpy((char*)wifi_config.ssid, ssid);
+  wifi_station_set_config(&wifi_config);
+  wifi_station_clear_cert_key();
+  wifi_station_clear_enterprise_ca_cert();
+  wifi_station_set_wpa2_enterprise_auth(1);
+  //wifi_station_set_enterprise_identity((uint8*)username, strlen(username));
+  wifi_station_set_enterprise_username((uint8*)username, strlen(username));
+  wifi_station_set_enterprise_password((uint8*)password, strlen(password));
+  wifi_station_connect();
+  //WiFi.begin("abcd", "6oM47!92");
+  Serial.println("Connecting");
   while (WiFi.status() != WL_CONNECTED) {
- 
-    delay(1000);
-    Serial.println("Connecting..");
+    delay(2000);
+    Serial.print(".");
  
   }
   Serial.println("Connected to WiFi Network");
